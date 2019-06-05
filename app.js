436456377b09b1ad4638003951c648ed8ee9399e
JulianaSinai <juianasinai1@gmail.com>
2019-06-05 13:49:51 -0300

    Add quem-somos.ejs

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.get("/", function(req, res){
  res.send("Página inicial");
 });
 
+app.get("/quem-somos", function(req, res){
+    res.send("História do projeto");
+});
+
+app.get("apadrinhamento", function(req, res){
+    res.send("Adquira sua vasilha");
+});
+
 app.listen(port, ip, function(){
     console.log("Pet-feeder foi inicializado...")
 });
