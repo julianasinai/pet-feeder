2795bbfbd665309fa1e9b1a6bb7ec743973ba254
Maria Luize <m.luize.s.pinheiro@gmail.com>
2019-06-05 20:05:27 -0300

    adicionar header & footer. Adicionar primeira versão de  página de apadrinhamento

--- app.js
+++ app.js
@@ -10,15 +10,15 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res){
- res.send("Página inicial");
+ res.render("home");
 });
 
 app.get("/quem-somos", function(req, res){
     res.render("quem-somos");
 });
 
-app.get("apadrinhamento", function(req, res){
-    res.send("Adquira sua vasilha");
+app.get("/apadrinhamento", function(req, res){
+    res.render("apadrinhamento");
 });
 
 app.listen(port, ip, function(){
