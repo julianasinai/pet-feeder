c49671fdd620f7893a0c73eb5e12085a90517a8a
Jean-mrs <jeanmoraes1994@hotmail.com>
2019-06-20 14:56:10 -0300

    integralização com protocolo

--- routes/mapa.js
+++ routes/mapa.js
@@ -4,24 +4,53 @@ const   express  = require("express"),
         // jquery   = require("jquery")
         router   = express.Router();//router = app
 
+        
 router.get("/", function(req,res){
-    const client  = mqtt.connect('mqtt://test.mosquitto.org')
+    var route_options={
+            clientId:"mqttjs01",
+            username:"lecircuit1",
+            password:"13bfed0eff764944a17783ac6d66b919",
+            clean:true};
+    
+    var count =0;
+    var client  = mqtt.connect("mqtt://io.adafruit.com",route_options);
+    console.log("connected flag  " + client.connected);
 
-    client.on('connect', function () {
-        client.subscribe('presence', function (err) {
-            if (!err) {
-            client.publish('presence', 'Hello mqtt')
-            }
+    //handle incoming messages
+    client.on('message',function(topic, message, packet){
+        console.log("message is "+ message);
+        console.log("topic is "+ topic);
+    });
+
+    client.on("connect",function(){	
+        console.log("connected  "+ client.connected);
         })
-    })
-
-    client.on('message', function (topic, message) {
-        // message is Buffer
-        console.log(message.toString())
-        client.end()
-    })
-
-
+        //handle errors
+        client.on("error",function(error){
+        console.log("Can't connect" + error);
+        process.exit(1)});
+    // //publish
+    // function publish(topic,msg,options){
+    //     console.log("publishing",msg);
+    //     if (client.connected == true){
+    //         client.publish(topic,msg,options);
+    //     }
+    //     count+=1;
+    //     if (count==2){ //ens script
+    //         clearTimeout(timer_id); //stop timer
+    //         client.end();
+    //     }	
+    // }
+    var options={
+        retain:true,
+        qos:1};
+    var topic="lecircuit1/feeds/force";
+    var message="test message";
+    console.log("subscribing to topics");
+    client.subscribe(topic,{qos:1}); //single topic
+    //var timer_id=setInterval(function(){publish(topic,message,options);},5000);
+    //notice this is printed even before we connect
+    console.log("end of script");
 
 //     // Generate a new random MQTT client id on each page load
 // const MQTT_CLIENT_ID = "iot_web_"+Math.floor((1 + Math.random()) * 0x10000000000).toString(16);
